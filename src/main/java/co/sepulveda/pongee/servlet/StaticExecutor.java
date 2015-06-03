c2afaa5c10d74b75d4c72b7b2b5f2a4a311c2438
Carlos Sepulveda <charlesandrew621@gmail.com>
2015-06-03 18:45:01 -0500

    Improve StaticExecutor
    - Remove unused imports

--- src/main/java/co/sepulveda/pongee/servlet/StaticExecutor.java
+++ src/main/java/co/sepulveda/pongee/servlet/StaticExecutor.java
@@ -5,6 +5,7 @@ import co.sepulveda.pongee.servlet.http.Response;
 import co.sepulveda.pongee.servlet.render.Render;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,15 +17,22 @@ public class StaticExecutor {
 
     public void execute(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String pathFile = req.getPathInfo().replace("/static/", "");
-        File file = new File("static/" + pathFile);
-        if (file.exists()) {
+        File file = getFile(pathFile);
+        if (file != null && file.exists()) {
             renderFile(file, req, resp);
         } else {
             renderNotFound(req, resp);
         }
-
     }
 
+    private File getFile(String pathFile) {
+        try {
+            URL urlFile = Thread.currentThread().getContextClassLoader().getResource("static/" + pathFile);
+            return new File(urlFile.getFile());
+        } catch (Exception e) {
+            return null;
+        }
+    }
     private void renderFile(File file, HttpServletRequest req, HttpServletResponse resp) throws IOException {
         Response response = new Response();
         response.setFile(file.getPath());
